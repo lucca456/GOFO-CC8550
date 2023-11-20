928370fcc6c66ce9c001f7c7e6323e6eed01a12c
Lucca Guarreschi <luccaguareschi@gmail.com>
2023-11-20 02:35:28 -0300

    PlaygroundTest updated

--- GOFO Implementation/src/test/java/System/PlaygroundTest.java
+++ GOFO Implementation/src/test/java/System/PlaygroundTest.java
@@ -1,4 +1,32 @@
 package System;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class PlaygroundTest {
+    private Playground playground;
+
+    @Before
+    public void setUp() {
+        playground = new Playground();
+    }
+
+    @Test
+    public void testSetAndGetCancellationPeriod() {
+        playground.setCancellationPeriod(24);
+        assertEquals("O período de cancelamento deve ser 24 horas", 24, playground.getCancellationPeriod());
+    }
+
+    @Test
+    public void testSetAndGetName() {
+        playground.setName("Playground Test");
+        assertEquals("O nome do playground deve ser 'Playground Test'", "Playground Test", playground.getName());
+    }
+
+    // Testes adicionais para os métodos que podem ser testados sem entrada do usuário
+    @Test
+    public void testSetAndGetOwner() {
+        playground.setOwner("Owner Name");
+        assertEquals("O proprietário deve ser 'Owner Name'", "Owner Name", playground.getOwner());
+    }
 }
