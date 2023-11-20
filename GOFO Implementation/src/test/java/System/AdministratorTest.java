cdcb4e3a1e85a4f418820577cb89a113e9b6019b
Lucca Guarreschi <luccaguareschi@gmail.com>
2023-11-20 02:25:35 -0300

    AdministratorTest updated

--- GOFO Implementation/src/test/java/System/AdministratorTest.java
+++ GOFO Implementation/src/test/java/System/AdministratorTest.java
@@ -1,2 +1,25 @@
-package System;public class AdministratorTest {
+package System;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AdministratorTest {
+    private Administrator admin;
+
+    @Before
+    public void setUp() {
+        admin = new Administrator();
+    }
+
+    @Test
+    public void testGetEmail() {
+        assertEquals("Email deve corresponder ao padrão", "admin@gmail.com", admin.getEmail());
+    }
+
+    @Test
+    public void testGetPassword() {
+        assertEquals("Senha deve corresponder ao padrão", "123", admin.getPassword());
+    }
 }
