00204fd4ba3ef5f1b895608a849299e7fd11e0b1
Lucca Guarreschi <luccaguareschi@gmail.com>
2023-11-20 02:47:52 -0300

    PlayGroundScheduleTest updated

--- GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
+++ GOFO Implementation/src/test/java/System/PlayGroundScheduleTest.java
@@ -1,4 +1,21 @@
 package System;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class PlayGroundScheduleTest {
+    private PlayGroundSchedule schedule;
+
+    @Before
+    public void setUp() {
+        schedule = new PlayGroundSchedule();
+        // Inicializar a agenda com configurações padrão, se necessário.
+    }
+
+    @Test
+    public void testSetAndGetTimeSlotPerHour() {
+        schedule.setPrice();
+        assertEquals("O preço por hora deve ser 50", 50, schedule.getTimeSlotPerHour());
+    }
 }
+
